87c5b64c6e1cc177aa546e9128546153ceea7bc0
Jake Mercer <jake.mercer@civica.co.uk>
2021-04-23 16:32:44 +0100

    Initial Commit
    Added a basic Zephyr OS-based blinky program targeted at the NXP
    MIMX1020 EVK.  Use `cmake -B build && make -C build flash` to build the
    program and flash it to the board.  Note that you must have followed the
    Zephyr OS getting started instructions and have the ZEPHYR_BASE
    environment variable set.  You will also need the JLink Host Debug Tools
    referenced in the MIMXRT1020 EVK documentation for Zephyr OS in order to
    flash the board via USB.

---
+++ src/main.c
@@ -0,0 +1,33 @@
+#include <drivers/gpio.h>
+
+#define SLEEP_TIME_MS 500
+
+#define LED0_NODE DT_ALIAS(led0)
+#define LED0 DT_GPIO_LABEL(LED0_NODE, gpios)
+#define PIN DT_GPIO_PIN(LED0_NODE, gpios)
+#define FLAGS DT_GPIO_FLAGS(LED0_NODE, gpios)
+
+void main (void)
+{
+#ifdef CONFIG_BOARD_MIMXRT1020_EVK
+    const struct device *dev;
+
+    if ((dev = device_get_binding(LED0)) == NULL)
+    {
+        return;
+    }
+
+    if (gpio_pin_configure(dev, PIN, GPIO_OUTPUT_ACTIVE | FLAGS) < 0)
+    {
+        return;
+    }
+#endif
+
+#ifdef CONFIG_BOARD_MIMXRT1020_EVK
+    while (true)
+    {
+        gpio_pin_set(dev, PIN, !gpio_pin_get(dev, PIN));
+        k_msleep(SLEEP_TIME_MS);
+    }
+#endif
+}
