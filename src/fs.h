9c33ab4172fe80374540f4298c1d45b0c8c5d722
Jake Mercer <jake.mercer@civica.co.uk>
2021-05-15 11:17:10 +0100

    FS/GPIO - Remove Extraneous #include

--- src/fs.h
+++ src/fs.h
@@ -1,8 +1,6 @@
 #ifndef __DUTCHESS_FS_H__
 #define __DUTCHESS_FS_H__
 
-#include <stdint.h>
-
 void dutchess_fs_init ();
 
 #endif // __DUTCHESS_FS_H__
--- src/gpio.h
+++ src/gpio.h
@@ -1,8 +1,6 @@
 #ifndef __DUTCHESS_GPIO_H__
 #define __DUTCHESS_GPIO_H__
 
-#include <stdint.h>
-
 void dutchess_gpio_init ();
 void dutchess_gpio_configure (int pin);
 void dutchess_gpio_set (int pin, int value);
